af87678e47b79adcb8f4500ce56be1c7ea8dab5a
Gaya3bnair <gayathribnair2004@gmail.com>
2023-06-01 21:39:06 +0530

    Translator

--- Web Corner/Translator CCube Easy/js/script.js
+++ Web Corner/Translator CCube Easy/js/script.js
@@ -14,7 +14,12 @@ translateBtn.addEventListener("click", () => {
     toText.setAttribute("placeholder", "Translating...");
     
     // Todo: add api fetch from https://mymemory.translated.net/doc/spec.php
-
+    let myurl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(myurl).then(res => res.json()).then(data =>{
+        console.log(data);
+        toText.value= data.responseData.translatedText;
+    });
+    
 });
 
 icons.forEach(icon => {
@@ -22,9 +27,25 @@ icons.forEach(icon => {
         if(!fromText.value || !toText.value) return;
         
         // TODO: add code to copy to Clipboard for the icon with class fa-copy
-
+        if (target.getAttribute("class") == "fas fa-copy") {
+            if (target.id == "from") {
+              navigator.clipboard.writeText(fromText.value);
+            } else {
+              navigator.clipboard.writeText(toText.value);
+            }
+          } else {
+            let talk;
+            if (target.id == "from") {
+              talk = new SpeechSynthesisUtterance(fromText.value);
+              talk.lang = selectTag[0].value;
+            } 
         // TODO: add code for speechSynthesis for icon with classname fa-volume-up (text to speech)
-
+        else {
+            talk = new SpeechSynthesisUtterance(toText.value);
+            talk.lang = selectTag[1].value;
+          }
+          speechSynthesis.speak(talk);
+        }
     });
 });
 
